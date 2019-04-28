b3c763937e8adb45d1ce953620786bb9487b5d71
Marek Rzepka <marku782@gmail.com>
2019-04-28 10:29:47 +0200

    add Clients Controller

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/clients', function() { return view('kontrahenci.index');});
+
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', 'UserController', ['except' => ['show']]);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
